658df9299607284cc33fe521301a629df8df57db
Dominik Hein <dominikhein1@gmx.net>
2026-01-18 13:33:49 +0100

    feat: Add initial seed data for exercises, muscles, equipment, body parts, and related entities.

---
+++ db/d1/data/exercise_type.sql
@@ -0,0 +1,8 @@
+INSERT INTO exercise_type (id, code, name_de, name_en) VALUES (1, 'STRENGTH', 'Kraft', 'Strength');
+INSERT INTO exercise_type (id, code, name_de, name_en) VALUES (2, 'CARDIO', 'Ausdauer', 'Cardio');
+INSERT INTO exercise_type (id, code, name_de, name_en) VALUES (3, 'STRETCHING', 'Dehnung', 'Stretching');
+INSERT INTO exercise_type (id, code, name_de, name_en) VALUES (4, 'PLYOMETRICS', 'Plyometrie', 'Plyometrics');
+INSERT INTO exercise_type (id, code, name_de, name_en) VALUES (5, 'POWERLIFTING', 'Kraftdreikampf', 'Powerlifting');
+INSERT INTO exercise_type (id, code, name_de, name_en) VALUES (6, 'STRONGMAN', 'Strongman', 'Strongman');
+INSERT INTO exercise_type (id, code, name_de, name_en) VALUES (7, 'OLYMPIC_WEIGHTLIFTING', 'Olympisches Gewichtheben', 'Olympic Weightlifting');
+INSERT INTO exercise_type (id, code, name_de, name_en) VALUES (8, 'WEIGHTLIFTING', 'WEIGHTLIFTING', 'WEIGHTLIFTING');
